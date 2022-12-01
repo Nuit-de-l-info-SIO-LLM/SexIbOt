987dd05b02d76628f663e63c85a0dbc6e1d360d8
Corentin384 <texiercorentin@orange.fr>
2022-12-01 21:51:27 +0100

    Ajout d'un fichier pour une connexion a la base de données

---
+++ db.js
@@ -0,0 +1,4 @@
+const mysql = require("mysql");
+const db = mysql.createConnection({   host: "localhost",   user: "root",   password: "" });
+db.connect(function(err) {   if (err) throw err;   console.log("Connecté à la base de données MySQL!"); });
+con.query
