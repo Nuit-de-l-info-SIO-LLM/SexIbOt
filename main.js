fed3d12eee70070e7d70c7e96cfd749e3dbc80a4
LucasR <Lucas.Ravagnier@etu.univ-grenoble-alpes.fr>
2022-12-02 01:00:06 +0100

    mise à jour

--- main.js
+++ main.js
@@ -24,15 +24,23 @@ BOT.on("messageCreate", (message) => {
         } else if (vart.length > 4) {
             message.channel.send("Trop d'arguments");
         } else {
+            // requêteSQL("SELECT ")
             if(message.author.id === vart[2] || message.author.username === vart[2]) {
                 message.channel.send("Vous n'avez pas le droit de faire cette commande sur vous...");
             } else if(vart[2].id === resultQuery || vart[2] === resultQuery) { // TODO : ResultQuery est une variable qui contient l'id de la personne qui a fait la demande
                 if(vart[3] === resultQueryType) { // TODO : ResultQueryType est une variable qui contient le type de carte, il doit correspondre.
                     if(vart[4] === resultQueryEnfant){
                         message.channel.send("Le joueur demande la carte : " + vart[4] + " pour la famille : " + vart[3] + " au joueur : " + vart[2]);
+                        if(resultQueryCarte === (vart[3]+"+"+vart[4])){ // TODO : Si l'utilisateur a la carte que le joueur a demandé, alors il doit lui donner.
+                            // TODO : Donner la carte a l'utilisateur l'ayant demandé et retiré la carte de la main du joueur visé.
+                        } else {
+                            message.channel.send("Le joueur ciblé n'a pas la carte demandé.");
+                        }
+                    } else{
+                        message.channel.send("La carte n'existe pas.");
                     }
                 } else {
-                    message.channel.send("Vous avez écrit de la merde");
+                    message.channel.send("Le type n'existe pas.");
                 }
             } 
         }
@@ -40,5 +48,15 @@ BOT.on("messageCreate", (message) => {
         message.reply("**Liste des commandes :**\n /demand {user} {type} {card}\n /help \n /creePartie")
     } else if(s.startsWith("/creePartie")) {
         message.reply("WIP !") // TODO : A faire
+    } else if(s.startsWith("/afficheCarte")) {
+        afficheCarte(message)
     }
 })
+
+function afficheCarte(message) {
+    message.channel.send("Voici vos cartes :")
+    message.channel.send("https://cdn.discordapp.com/attachments/1047875519787585598/1047999111619674203/gonorrhee-8.jpg")
+}
+function requêteSQL(query) {
+    return dbConnexion.SQLQuery(query);
+}
